80889ebdab41c8169895e6356c2ddc96409f1166
Meshv Patel <meshv1823@gmail.com>
2022-11-08 19:02:47 +0530

    Solve img issue.

--- config/plugins.js
+++ config/plugins.js
@@ -23,4 +23,19 @@ module.exports = ({ env }) => ({
       smtpHost: "localhost", // Default: -1 - extra smtp host after resolveMX
     },
   },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
 });
